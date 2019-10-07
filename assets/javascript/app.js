8aed41ac68fb4583ed68021358a20706546db711
Omar Luna <mrebel62@gmail.com>
2019-10-07 16:38:51 -0500

    basic structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var questions = [
+        "What is the main ingredient of Bombay Duck?",
+        "How long is New Zealand's Ninety Mile Beach?",
+        "Which country was the Caesar salad invented in?",
+        "How many months have 28 days in them?",
+        "What color are aircraft black boxes?",
+        "What kind of animal is a prairie dog?",
+        "After which animal are the Canary Islands named?",
+        "How long did the 100 years war last?",
+        "In which country are Panama hats made?",
+        "From which country do French fries originate?"
+    ];
+
+    var answers = [
+        ["Fish","Duck","Beef","Pork"],
+        ["99 miles","100 miles"," 55 miles","110 miles"],
+        ["Italy","USA","France","Mexico"],
+        ["1","12","3","5"],
+        ["Black","Orange","Purple","Metallic (No color)"],
+        ["Rodent","Canine","Feline","Bird"],
+        ["Canary","Dog","Cat","Tiger"],
+        ["99 years","100 years","105 years","116 years"],
+        ["Ecuador","Panama","Colombia","Venezuela"],
+        ["Belgium","France","USA","China"],
+    ];
+
+    var correctAnswers = [0,2,3,2,2,0,1,3,1,0];
+
+})
