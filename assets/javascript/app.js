1d0abad0546fcc0185165a7ebb9c86f4af229457
Omar Luna <mrebel62@gmail.com>
2019-10-07 23:35:24 -0500

    logic correction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,6 @@ $(document).ready(function(){
     ];
 
     var correctAnswers = [0,2,3,2,2,0,1,3,1,0];
-    var selectedAnswers = [];
     var score = 0;
     var currentQuestion = 0;
 
@@ -48,32 +47,39 @@ $(document).ready(function(){
         if (!clockRunning) {
             intervalId = setInterval(count,1000);
             clockRunning = true;
-          }
+        }
     }
     function stop(){
         clearInterval(intervalId);
+        time = 10;
         clockRunning = false;
     }
 
     function timeConverter(t) {
 
-        //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
-        var minutes = Math.floor(t / 60);
-        var seconds = t - (minutes * 60);
-      
-        if (seconds < 10) {
-          seconds = "0" + seconds;
+        //((  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+        //console.log(t);
+        if(t === 0){
+          timeEnded();
+        }else{
+
+          var minutes = Math.floor(t / 60);
+          var seconds = t - (minutes * 60);
+        
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+        
+          if (minutes === 0) {
+            minutes = "00";
+          }
+        
+          else if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+        
+          return minutes + ":" + seconds;
         }
-      
-        if (minutes === 0) {
-          minutes = "00";
-        }
-      
-        else if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
-      
-        return minutes + ":" + seconds;
       }
 
       //Function to start the actual game
@@ -88,18 +94,38 @@ $(document).ready(function(){
               $("#option"+(i+1)).text(answers[index][i]);
               $("#option"+(i+1)).attr("val",i);
           }
+          start();
       }
 
       $(".option").on("click", function(){
-        console.log("Clicked val " + $(this).attr("val"));
-        var selectedAnswer = $(this).attr("val");
-        selectedAnswers.push[selectedAnswer];
-        if(selectedAnswers.indexOf(selectedAnswer)){
-            score++;
-            console.log(score);
-        }
+        var selectedAnswer = parseInt($(this).attr("val"));
+        checkAnswer(selectedAnswer);
         currentQuestion += 1;
+        stop(); 
         populateGame(currentQuestion);
       });
+
+      function checkAnswer(answer){
+        if(answer === correctAnswers[currentQuestion]){
+          createModal(true);
+          score += 1;
+        }else{
+          createModal(false)
+        }
+      }
+
+      function createModal(option){
+        if(option){
+          $(".modal-title").text("Correct!");
+          $(".modal-body").text("That's right! your current score is:" + score);
+        }else{
+          $(".modal-title").text("Inorrect!");
+          $(".modal-body").text("Awww! Better luck next time");
+        }
+      }
+
+      function timeEnded(){
+        $("#btnTrigger").click();
+      }
 
 })
